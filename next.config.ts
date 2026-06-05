11bace67633878a889c287602784e556e5fdba16
raherinjaka <raherinjakat@gmail.com>
2026-06-05 17:09:01 +0300

    fix error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',      // <--- INDISPENSABLE : Génère le dossier /out pour Capacitor
   images: {
     unoptimized: true,   // <--- INDISPENSABLE : Pour que tes images s'affichent sur Android
   },
